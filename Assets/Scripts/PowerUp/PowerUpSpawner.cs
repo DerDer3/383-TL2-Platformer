cd68d68977d6a92937271616818afa01c1cd3757
Yoshiinator <joshuawerner98@outlook.de>
2025-09-10 18:43:38 -0700

    hotfix for scene and spawn with player

--- Assets/Scripts/PowerUp/PowerUpSpawner.cs
+++ Assets/Scripts/PowerUp/PowerUpSpawner.cs
@@ -9,19 +9,24 @@ public class PowerUpSpawner : MonoBehaviour
     public List<Vector3> spawnPositions;
 
     private int indexSpawnpoint = 0;
+    private bool[] hasSpawned;
 
     public GenericSpawner genericSpawner;
 
+    private void Start()
+    {
+        hasSpawned = new bool[spawnX.Count];
+    }
+
     private void Update()
     {
-        bool[] hasSpawned = new bool[spawnPositions.Count];
-
-        for(int i = 0 ; i < powerupPrefabs.Count;i++)
+        for(int i = 0; i < spawnX.Count; i++)
         {
             if (!hasSpawned[i] && player.transform.position.x > spawnX[i])
             {
                 SpawnRandomPowerup();
                 hasSpawned[i] = true;
+                break;
             }
         }
     }
@@ -29,12 +34,14 @@ public class PowerUpSpawner : MonoBehaviour
     void SpawnRandomPowerup()
     {
         if(powerupPrefabs.Count == 0 || spawnPositions.Count == 0 || genericSpawner == null) { return; }
+        if (indexSpawnpoint >= spawnPositions.Count) { return; }
 
         int prefabsindex = Random.Range (0, powerupPrefabs.Count);
         //int indexSpawnpoint = Random.Range(0, spawnPositions.Count);
 
         genericSpawner.Spawn(powerupPrefabs[prefabsindex], spawnPositions[indexSpawnpoint], Quaternion.identity);
         indexSpawnpoint += 1;
+
     }
 }
 
